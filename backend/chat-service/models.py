069026219f357b38b3e558b7f333ec0fd0e81927
Mintori <1207cms@naver.com>
2025-06-12 14:20:48 +0900

    쓰잘때기 없는거 삭제

--- backend/chat-service/models.py
+++ backend/chat-service/models.py
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 # backend/chat-service/models.py
 
-from sqlalchemy import Column, Integer, String, DateTime, Text
-from sqlalchemy.sql import func
-from .database import Base
-=======
 from sqlalchemy import Column, Integer, String, Text, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
->>>>>>> 4364be74933165036c85e9fd06094caaf0309666
 
 class Chat(Base):
     __tablename__ = "chats"
